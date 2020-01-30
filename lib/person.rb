a9f19ab16aba5171d36f55d28022ab80c41f9d66
Chris Ashman <chrisashman96@gmail.com>
2020-01-30 20:17:20 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person 
+  
+  def talk 
+    puts "Hello World!"
+  end
+  
+  def walk 
+    puts "The Person is walking"
+  end
+end
+
+alan_key = Person.new 
+
+alan_key.talk
+alan_key.walk
