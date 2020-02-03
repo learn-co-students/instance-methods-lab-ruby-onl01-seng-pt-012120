538bf35d0cb8e029e9f4da66f057fa9ad9ce0662
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-02-03 23:09:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+end
+ 
+rog = Dog.new 
+rog.methods
+
+
+
+class Dog
+  
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+# fido = Dog.new
+# fido.sit
+
+class Dog
+  
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+ 
+ 
