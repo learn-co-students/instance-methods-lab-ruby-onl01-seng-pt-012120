23125a315ef4e8440b44d21b96cb7c9be2e58869
Patricia C <patricia@patriciaco.com>
2020-02-04 03:43:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+
+# class Baby 
+# def initalize
+#   cry
+# end
+
+# def cry #instance method
+#   puts "Waaaaaa"
+# end 
+# end
+
+# north_west = Baby.new 
+# north.west.cry
+
+# class Dog
+# puts "woof"
+# end
+
+
+class Dog
+end
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+
+
+
+
+
